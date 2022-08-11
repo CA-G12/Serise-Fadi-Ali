872b274d9f47f6bedca6c36f88bec8d88f1936a6
fadezak100 <fadyzaqut@gmail.com>
2022-08-11 16:58:47 +0300

    Serve index.html for landing page and its static files and write tests for this route Relates #22

--- test/test.test.js
+++ test/test.test.js
@@ -1,8 +1,27 @@
+/* eslint-disable no-undef */
 const supertest = require('supertest');
 const router = require('../src/routes');
 
 test('(/) Main route returns 200', (done) => {
-  supertest(router).get('/').expect(200).expect('Content-Type', /html/)
+  supertest(router).get('/').expect(200).expect('Content-Type', 'text/html')
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+});
+
+test('(/public/app.js) returns the js file (/) page', (done) => {
+  supertest(router).get('/public/app.js').expect(200).expect('Content-Type', 'text/js')
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+});
+
+test('(/public/app.css) returns the css file for (/) page', (done) => {
+  supertest(router).get('/public/app.css').expect(200).expect('Content-Type', 'text/css')
     .end((err, res) => {
       if (err) return done(err);
       expect(res.statusCode).toBe(200);
