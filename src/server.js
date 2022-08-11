c6b40f6f924a75358a9e5e232abdb9bd9d57bbe9
fadezak100 <fadyzaqut@gmail.com>
2022-08-11 16:59:34 +0300

    Edit PORTT typo in server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,6 @@ const router = require('./routes');
 
 const server = http.createServer(router);
 
-server.listen(process.env.PORTT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
